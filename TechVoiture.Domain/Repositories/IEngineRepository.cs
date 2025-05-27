ee775e74d43a783317010eda1a9c93209cf3fb1b
Balthazar Picsou <balthazar.picsou@outlook.be>
2025-05-27 15:43:18 +0200

    Repostories interfaces 🙀

---
+++ TechVoiture.Domain/Repositories/IEngineRepository.cs
@@ -0,0 +1,13 @@
+﻿using TechVoiture.Domain.Models;
+
+namespace TechVoiture.Domain.Repositories
+{
+    public interface IEngineRepository
+    {
+        IEnumerable<Engine> FindAll();
+        Engine Find(int id);
+        Engine Create(Engine engine);
+        Engine Update(Engine engine);
+        bool Delete(int id);
+    }
+}
